139de72bf438d55f32edcd8c21554f1e8e48d4c6
mahdibayat73 <mahdi.riraco@gmail.com>
2023-11-06 14:12:38 +0330

    Checking authorized files for uploading and creating upload folder and receiving authorized files

--- helper/file-upload.php
+++ helper/file-upload.php
@@ -6,14 +6,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST["uploadBtn"]) && $_POST["uploadBtn"] == "Upload") {
         if (isset($_FILES["uploadedFile"]) && !empty($_FILES["uploadedFile"]["name"])) {
             // var_dump($_FILES["uploadedFile"]);
-            $_FILES["uploadedFile"]["name"];
-            $_FILES["uploadedFile"]["size"];
-            $_FILES["uploadedFile"]["type"];
+            $fileName = $_FILES["uploadedFile"]["name"];
+            $fileSize = $_FILES["uploadedFile"]["size"];
+            $fileType = $_FILES["uploadedFile"]["type"];
+            $filePath = $_FILES["uploadedFile"]["tmp_name"];
+            $fileNameSeprate = explode(".", $fileName);
+            $fileExtention = strtolower(end($fileNameSeprate));
+            $newFileName = md5(time() . $fileName) . "." . $fileExtention;
+            $allowedFileExtentions = ["jpg", "jpeg", "png"];
+            if (in_array($fileExtention, $allowedFileExtentions)) {
+                $uploadFileDir = "../upload/";
+                $destPath = $uploadFileDir . $newFileName;
+                if (move_uploaded_file($filePath, $destPath)) {
+                    $msg = "Your file has been successfully uploaded.";
+                } else {
+                    $msg = "Error uploading the file!";
+                }
+            } else {
+                $msg = "The file you want to upload is not allowed!";
+            }
         } else {
-            echo $msg = "Please select the file you want!";
+            $msg = "Please select the file you want!";
         }
     }
 }
 
 $_SESSION["msg"] = $msg;
-// header("location:../index.php");
+header("location:../index.php");
