b913f8ef20b55bbebd952e3857d653a3f13636f4
mahdibayat73 <mahdi.riraco@gmail.com>
2023-11-06 15:03:32 +0330

    Edited some html and css codes

--- index.php
+++ index.php
@@ -13,25 +13,27 @@ session_start();
 </head>
 
 <body>
-    <div class="container">
+    <main>
+        <div class="container">
 
-        <?php if (isset($_SESSION["msg"]) && $_SESSION["msg"]) : ?>
-            <p class="msg">
-                <?php echo $_SESSION["msg"]; ?>
-            </p>
-            <?php unset($_SESSION["msg"]); ?>
-        <?php endif ?>
+            <?php if (isset($_SESSION["msg"]) && $_SESSION["msg"]) : ?>
+                <p class="msg">
+                    <?php echo $_SESSION["msg"]; ?>
+                </p>
+                <?php unset($_SESSION["msg"]); ?>
+            <?php endif ?>
 
-        <form method="POST" action="helper/file-upload.php" enctype="multipart/form-data">
-            <div class="upload-wrapper">
-                <span class="file-name">Choose a file...</span>
-                <label for="file-upload">Browse
-                    <input type="file" id="file-upload" name="uploadedFile">
-                </label>
-            </div>
-            <input type="submit" name="uploadBtn" value="Upload" />
-        </form>
-    </div>
+            <form method="POST" action="helper/file-upload.php" enctype="multipart/form-data">
+                <div class="upload-wrapper">
+                    <span class="file-name">Choose a file...</span>
+                    <label for="file-upload">Browse
+                        <input type="file" id="file-upload" name="uploadedFile">
+                    </label>
+                </div>
+                <input type="submit" name="uploadBtn" value="Upload" />
+            </form>
+        </div>
+    </main>
 </body>
 
 </html>
